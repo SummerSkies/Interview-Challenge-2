ced56a0e67d670be08ae421692e1ce2000fac84d
Juliana Nielson <nielsonjuliana@gmail.com>
2023-07-20 18:26:50 -0600

    Added functionality to the Number Of Users label and stepper.

--- GenerationSettingsView.swift
+++ GenerationSettingsView.swift
@@ -8,6 +8,18 @@
 import UIKit
 
 class GenerationSettingsView: UIViewController {
+    @IBOutlet weak var userNumberLabel: UILabel!
+    @IBOutlet weak var userNumberStepper: UIStepper!
+    @IBOutlet weak var genderCheckbox: UIButton!
+    @IBOutlet weak var emailCheckbox: UIButton!
+    @IBOutlet weak var ageCheckbox: UIButton!
+    @IBOutlet weak var birthdayCheckbox: UIButton!
+    
+    var numberOfUsers = 0 {
+        didSet {
+            print(numberOfUsers)
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,5 +34,20 @@ class GenerationSettingsView: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
-
+    @IBAction func stepperValueChanged(_ sender: UIStepper) {
+        guard let numberOfUsers = userNumberLabel.text else { return userNumberLabel.text = "0" }
+        guard let currentNumberOfUsers = Int(numberOfUsers) else { return userNumberLabel.text = "0" }
+        
+        var previousStepperValue = self.numberOfUsers
+        
+        if userNumberStepper.value > Double(previousStepperValue) {
+            userNumberStepper.value = Double(currentNumberOfUsers) + userNumberStepper.stepValue
+            userNumberLabel.text = String(Int(userNumberStepper.value))
+        } else {
+            userNumberStepper.value = Double(currentNumberOfUsers) - userNumberStepper.stepValue
+            userNumberLabel.text = String(Int(userNumberStepper.value))
+        }
+        self.numberOfUsers = Int(userNumberStepper.value)
+        return
+   }
 }
